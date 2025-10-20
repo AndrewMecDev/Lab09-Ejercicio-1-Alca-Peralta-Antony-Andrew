f67cfa12f927409905fbe0f67a09af9545c16a08
AndrewMecDev <antonyalca.pe@gmail.com>
2025-10-19 23:33:52 -0500

    Agregando el permiso para acceder a internet

--- app/src/main/java/com/example/lab09ejerc1/model/Product.kt
+++ app/src/main/java/com/example/lab09ejerc1/model/Product.kt
@@ -1,13 +1,3 @@
 package com.example.lab09ejerc1.model
 
 // Representa un único producto dentro de un carrito.
-data class Product(
-    val id: Int,
-    val title: String,
-    val price: Double,
-    val quantity: Int,
-    val total: Double,
-    val discountPercentage: Double,
-    val discountedPrice: Double,
-    val thumbnail: String // URL de la imagen miniatura del producto
-)
