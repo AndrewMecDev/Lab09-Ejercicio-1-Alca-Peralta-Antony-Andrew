e920c898d4c1e8d245df9d242e35d725c87417fb
AndrewMecDev <antonyalca.pe@gmail.com>
2025-10-19 23:35:27 -0500

    Agregando y modificando CartsApiResponse.kt

--- app/src/main/java/com/example/lab09ejerc1/model/CartsApiResponse.kt
+++ app/src/main/java/com/example/lab09ejerc1/model/CartsApiResponse.kt
@@ -1,3 +1,9 @@
 package com.example.lab09ejerc1.model
 
 // Representa la respuesta completa de la API, que contiene una lista de carritos.
+data class CartsApiResponse(
+    val carts: List<Cart>, // La lista principal de carritos
+    val total: Int,
+    val skip: Int,
+    val limit: Int
+)
